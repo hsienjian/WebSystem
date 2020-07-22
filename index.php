fa683e1b279ec2756f49b7c2f5aca925d92453b4
Forge-15 <Forge-15@DESKTOP-A3L59BB>
2020-07-22 19:14:56 +0800

    nav & img

--- index.php
+++ index.php
@@ -8,21 +8,26 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        
     </head>
-    
+    <!-- Font Awesome JS -->
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+        <link href="index.css" rel="stylesheet">
     <body>
         <nav class="site-header sticky-top py-1">
   <div class="container d-flex flex-column flex-md-row justify-content-between">
     <a class="py-2" href="#" aria-label="Product">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24" focusable="false"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+        
+        <img src="img/myprinting.png" alt="product" width="220px" height="60px" style="margin-top:  -5px"></img>
     </a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Home</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">About us</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">All Product </a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Service</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Contact us</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">My Account</a>
+    <a class="py-2 d-none d-md-inline-block" href="#" style="margin-top:  12px">Home</a>
+    <a class="py-2 d-none d-md-inline-block" href="#" style="margin-top:  12px">About us</a>
+    <a class="py-2 d-none d-md-inline-block" href="#" style="margin-top:  12px">All Product </a>
+    <a class="py-2 d-none d-md-inline-block" href="#" style="margin-top:  12px">Service</a>
+    <a class="py-2 d-none d-md-inline-block" href="#" style="margin-top:  12px">Contact us</a>
+    <a class="py-2 d-none d-md-inline-block" href="#" style="margin-top:  12px">Cart</a>
+    <a class="py-2 d-none d-md-inline-block" href="#" style="margin-top:  12px"><i class="fas fa-user-circle"></i><span>&numsp;</span>My Account</a>
   </div>
 </nav>
         
