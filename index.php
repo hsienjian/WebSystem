881452b83854ba6d3e2803d20aa0a331f4af6ca0
Forge-15 <Forge-15@DESKTOP-A3L59BB>
2020-07-22 17:35:01 +0800

    footer nav

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    
+    <body>
+        <nav class="site-header sticky-top py-1">
+  <div class="container d-flex flex-column flex-md-row justify-content-between">
+    <a class="py-2" href="#" aria-label="Product">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24" focusable="false"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+    </a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Home</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">About us</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">All Product </a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Service</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Contact us</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">My Account</a>
+  </div>
+</nav>
+        
+        <?php
+        echo 'hello world, good night';
+        include('footer.html');
+        ?>
+    </body>
+    
+</html>
