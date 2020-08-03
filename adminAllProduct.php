b9b761da1fba77e7e026b07d67ba79c2cb62bf8e
Forge-15 <Forge-15@DESKTOP-A3L59BB>
2020-08-03 19:35:28 +0800

    admin all product database done

---
+++ adminAllProduct.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>All Product</title>
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+  
+   
+              <!-- Our Custom CSS -->
+        <link rel="stylesheet" href="css/style.css">
+        <!-- Font Awesome JS -->
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+    </head>
+<body>
+    <div class="wrapper">
+        <?php
+        include('includes/sidebar.php'); 
+        include('includes/adminnav.php'); 
+        ?>
+
+        <nav>[All Product | Insert Product ]</nav>
+        <h3>List Student</h3>
+        <table>
+            <tr><th>Product ID</th><th>Product Name</th><th>Price</th><th>Status</th></tr>
+            
+            <?php
+            $conn = new mysqli('localhost','root','','assignment');
+            $sql = "select * from allproduct";
+            $result = $conn ->query($sql);
+            while($row=$result->fetch_assoc()){
+                echo "<tr><td>{$row['ProductID']}</td><td>{$row['ProductName']}</td><td>{$row['Price']}</td><td>{$row['Status']}</td></tr>";
+            
+               
+                 
+               
+               
+                
+                
+                }
+            
+            
+            
+            $conn->close();
+            ?>
+        </table>
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+    
+    
+</body>
+    
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+    
+</html>
